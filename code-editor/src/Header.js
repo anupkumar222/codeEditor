e07364e6ac7c73b36ca6a351ede3b85bb3f2559c
anup kumar <kanup414@gmail.com>
2023-03-11 15:10:30 +0530

    working

---
+++ code-editor/src/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+    return(
+        <header>
+            <div className="text-center p-2">
+                <h1 className="text-3xl font-medium">Code Editor</h1>
+                <p className="text-sm m-4">You can write and preview HTML and CSS!</p>
+            </div>
+        </header>
+    )
+}
+export default Header
